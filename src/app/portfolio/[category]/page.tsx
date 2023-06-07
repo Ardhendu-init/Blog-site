1e10b0b775f4236ca51cfdce23ff1ad814b14e4a
ardhendu_init_ <ardhendujj002@gmail.com>
2023-06-07 12:10:06 +0530

    Individual Category page has been added

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import styles from "./page.module.css";
+import Button from "@/src/components/button/Button";
+import Image from "next/image";
 
-const Category = () => {
-  return <div>Category</div>;
+const data = [
+  {
+    id: 1,
+    title: "Creative Portfolio",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+    image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+  },
+  {
+    id: 2,
+    title: "Minimal Single Product",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+    image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+  },
+  {
+    id: 3,
+    title: "Strong Together Charity",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+    image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+  },
+];
+
+const Category: React.FC<{ params: any }> = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.category}>{params.category}</h2>
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button title="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} fill={true} src={item.image} alt="" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
-const Portfolio = () => {
-  return <div>Portfolio</div>;
+const galleryContent = [
+  {
+    id: 1,
+    name: "Illustrations",
+    link: "/portfolio/illustrations",
+  },
+  {
+    id: 2,
+    name: "Websites",
+    link: "/portfolio/websites",
+  },
+  {
+    id: 3,
+    name: "Application",
+    link: "/portfolio/application",
+  },
+];
+
+const Portfolio: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        {galleryContent.map((item) => (
+          <Link key={item.id} href={item.link} className={styles.item}>
+            <span className={styles.title}>{item.name}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
