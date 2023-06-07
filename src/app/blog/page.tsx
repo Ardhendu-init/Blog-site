c9ff4cdb8b56cae33ac1cf05f9a36d2733e444f9
ardhendu_init_ <ardhendujj002@gmail.com>
2023-06-07 12:41:04 +0530

    Blog pages has been added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,73 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/id" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            nam sit deleniti praesentium quae, vel error impedit, atque est
+            cupiditate alias! Corporis deserunt inventore aperiam quasi eveniet
+            accusamus aliquam pariatur?
+          </p>
+        </div>
+      </Link>
+      <Link href="/blog/id" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            nam sit deleniti praesentium quae, vel error impedit, atque est
+            cupiditate alias! Corporis deserunt inventore aperiam quasi eveniet
+            accusamus aliquam pariatur?
+          </p>
+        </div>
+      </Link>
+      <Link href="/blog/id" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            nam sit deleniti praesentium quae, vel error impedit, atque est
+            cupiditate alias! Corporis deserunt inventore aperiam quasi eveniet
+            accusamus aliquam pariatur?
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
